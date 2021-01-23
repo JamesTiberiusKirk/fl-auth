aafe81c6345a444f40cc4a7c6a95555f527b67c4
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-01-23 17:54:44 +0000

    Cleaned up env.go

--- config/env.go
+++ config/env.go
@@ -17,18 +17,16 @@ type Env struct {
 	SERVER_PORT string
 }
 
-func GetEnv() (Env, error) {
+func GetEnv() Env {
 
 	envConfig := Env{}
 
-	// load .env file from given path
-	// we keep it empty it will load .env from current directory
 	err := godotenv.Load(".env")
 
 	if err != nil {
 		fmt.Println("No .env file")
-		// log.Fatalf("Error loading .env file")
-		// return envConfig, err
+	} else {
+		fmt.Print("Loading from .env..")
 	}
 
 	envConfig.DB_HOST = os.Getenv("DB_HOST")
@@ -38,8 +36,5 @@ func GetEnv() (Env, error) {
 	envConfig.DB_PASSWORD = os.Getenv("DB_PASSWORD")
 	envConfig.SERVER_PORT = os.Getenv("SERVER_PORT")
 
-	fmt.Println(envConfig)
-
-	return envConfig, nil
-
+	return envConfig
 }
