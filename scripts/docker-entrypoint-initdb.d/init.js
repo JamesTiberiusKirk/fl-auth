1c781dae9605c226407e49a1dafaa042a0e07129
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-02-18 17:32:30 +0000

    Added ci-cd script

--- scripts/docker-entrypoint-initdb.d/init.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB('fl-auth')
-db.createUser(
-{
-  user: "fl-auth",
-  pwd: "fl-auth123", 
-  roles: [
-    { role: "readWrite", db: "fl-auth" }
-  ]
-})
--- scripts/initdb.d/init.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB('users')
-db.createUser(
-{
-  user: "fl-auth",
-  pwd: "fl-auth123",
-  roles: [
-    { role: "readWrite", db: "users" }
-  ]
-})
